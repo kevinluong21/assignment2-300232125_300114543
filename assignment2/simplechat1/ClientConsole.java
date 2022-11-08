410523a74a8b7c8eddbc11f42a21d813cda58750
Kevin Luong <64666314+kevinluong21@users.noreply.github.com>
2022-11-07 21:25:05 -0500

    Add files via upload

---
+++ assignment2/simplechat1/ClientConsole.java
@@ -0,0 +1,197 @@
+// This file contains material supporting section 3.7 of the textbook:
+// "Object Oriented Software Engineering" and is issued under the open-source
+// license found at www.lloseng.com 
+
+import java.io.*;
+
+import client.ChatClient;
+import common.*;
+import ocsf.client.*;
+
+/**
+ * This class constructs the UI for a chat client.  It implements the
+ * chat interface in order to activate the display() method.
+ * Warning: Some of the code here is cloned in ServerConsole 
+ *
+ * @author Fran&ccedil;ois B&eacute;langer
+ * @author Dr Timothy C. Lethbridge  
+ * @author Dr Robert Lagani&egrave;re
+ * @version July 2000
+ */
+public class ClientConsole implements ChatIF 
+{
+  //Class variables *************************************************
+  
+  /**
+   * The default port to connect on.
+   */
+  final public static int DEFAULT_PORT = 7125; // **** Changed for E49 KL (7 + last 3 digits of student number)
+  
+  //Instance variables **********************************************
+  
+  /**
+   * The instance of the client that created this ConsoleChat.
+   */
+  ChatClient client;
+
+  
+  //Constructors ****************************************************
+
+  /**
+   * Constructs an instance of the ClientConsole UI.
+   *
+   * @param host The host to connect to.
+   * @param port The port to connect on.
+   */
+  public ClientConsole(String host, int port) 
+  {
+    try 
+    {
+      client= new ChatClient(host, port, this);
+    } 
+    catch(IOException exception) 
+    {
+      System.out.println("Error: Can't setup connection!"
+                + " Terminating client.");
+      System.exit(1);
+    }
+  }
+
+  
+  //Instance methods ************************************************
+  
+  /**
+   * This method waits for input from the console.  Once it is 
+   * received, it sends it to the client's message handler.
+   */
+  public void accept() 
+  {
+    try
+    {
+      BufferedReader fromConsole = 
+        new BufferedReader(new InputStreamReader(System.in));
+      String message;
+
+      while (true) 
+      {
+        message = fromConsole.readLine();
+
+        if (message.trim().charAt(0) == '#') { // **** Changed for E50 KL
+
+          String[] splitMessage = message.split(" "); //splits message by the space if a port number or host name is specified as well
+
+          switch(splitMessage[0].substring(1)) {
+            case "quit":
+              System.out.println("Terminating client...");
+              client.quit();
+              break;
+            case "logoff":
+              System.out.println("Logging off of server...");
+              client.closeConnection();
+              break;
+            case "sethost":
+              if (splitMessage.length == 2) { //if the message has a length of 2, then it means a host name was specified
+                if (!client.isConnected()) { //client cannot be connected while setting host
+                  System.out.println("Host was set successfully.");
+                  client.setHost(splitMessage[1]);
+                }
+                else {
+                  System.out.println("ERROR: Cannot set host while client is connected.");
+                }
+              }
+              else {
+                System.out.println("ERROR: No host specified.");
+              }
+              break;
+            case "setport":
+              if (splitMessage.length == 2) { //if the message has a length of 2, then it means a port number was specified
+                if (!client.isConnected()) { //client cannot be connected while setting port
+                  try {
+                    int portFromMessage = Integer.parseInt(splitMessage[1]);
+                    System.out.println("Port was set successfully. Remember to login again to use the server.");
+                    client.setPort(portFromMessage);
+                  }
+                  catch (NumberFormatException e) {
+                    System.out.println("ERROR: Port must be an int.");
+                  }
+                }
+                else {
+                  System.out.println("ERROR: Cannot set port while client is connected.");
+                }
+              }
+              else {
+                System.out.println("ERROR: No port specified.");
+              }
+              break;
+            case "login":
+              if (!client.isConnected()) { //client does not need to open connection again if it is already connected
+                System.out.println("Client is now connected.");
+                client.openConnection();
+              }
+              else {
+                System.out.print("ERROR: Client already connected.");
+              }
+              break;
+            case "gethost":
+              System.out.println(client.getHost());
+              break;
+            case "getport":
+              System.out.println(client.getPort());
+              break;
+          }
+        }
+        else { // **** Changed for E50 KL
+          client.handleMessageFromClientUI(message);
+        }
+      }
+    } 
+    catch (Exception ex) 
+    {
+      System.out.println
+        ("Unexpected error while reading from console!");
+    }
+  }
+
+  /**
+   * This method overrides the method in the ChatIF interface.  It
+   * displays a message onto the screen.
+   *
+   * @param message The string to be displayed.
+   */
+  public void display(String message) // **** Changed for E50 KL
+  {
+    if (message.contains("SERVER MSG> ")) { //if the message contains SERVER MSG>, it came from the server
+      System.out.println(message);
+    }
+    else {
+      System.out.println("> " + message); //message is an echo from the server
+    }
+  }
+
+  
+  //Class methods ***************************************************
+  
+  /**
+   * This method is responsible for the creation of the Client UI.
+   *
+   * @param args[0] The host to connect to.
+   */
+  public static void main(String[] args) 
+  {
+    String host = "";
+    int port = DEFAULT_PORT;  //The port number // **** Changed for E50 KL
+
+    try
+    {
+      host = args[0];
+      port = Integer.parseInt(args[1]); // **** Changed for E50 KL
+    }
+    catch(ArrayIndexOutOfBoundsException e)
+    {
+      host = "localhost";
+    }
+    ClientConsole chat= new ClientConsole(host, port); // **** Changed for E50 KL
+    chat.accept();  //Wait for console data
+  }
+}
+//End of ConsoleChat class
---
+++ assignment2/simplechat1/ServerConsole.java
@@ -0,0 +1,100 @@
+// **** Class created for E50 KL
+
+import java.io.*;
+import common.*;
+import ocsf.server.*;
+
+public class ServerConsole implements ChatIF {
+
+  EchoServer server;
+
+  public ServerConsole(int port) {
+    server = new EchoServer(port);
+
+    try {
+      server.listen(); // Start listening for connections
+
+    } catch (Exception ex) {
+      System.out.println("ERROR - Could not listen for clients!");
+    }
+  }
+
+  public void accept() {
+    try {
+      BufferedReader fromConsole = new BufferedReader(new InputStreamReader(System.in));
+      String message;
+
+      while (true) {
+        message = fromConsole.readLine();
+
+        if (message.trim().charAt(0) == '#') { // **** Changed for E50 KL
+
+          String[] splitMessage = message.split(" "); //splits message by space in the cases for specifying a port number
+
+          switch (splitMessage[0].substring(1)) {
+            case "quit":
+              System.out.println("Terminated server...");
+              System.exit(0); //quits program
+              break;
+            case "stop":
+              System.out.println("No longer accepting new connections...");
+              server.stopListening();
+              break;
+            case "close":
+              server.close();
+              break;
+            case "setport":
+              if (splitMessage.length == 2) { //if message is length 2, then there is most likely a port number specified
+                if (!server.isListening()) { //sets port number only when the server is not listening
+                  try {
+                    int portFromMessage = Integer.parseInt(splitMessage[1]);
+                    System.out.println("Port was set successfully.");
+                    server.setPort(portFromMessage);
+                  } catch (NumberFormatException e) {
+                    System.out.println("ERROR: Port must be an int.");
+                  }
+                } else {
+                  System.out.println("ERROR: Cannot set port while server is listening.");
+                }
+              } else {
+                System.out.println("ERROR: No port specified.");
+              }
+              break;
+            case "start":
+              if (!server.isListening()) { //if server is already listening, it does not need to start again
+                System.out.println("Server is now connected.");
+                server.listen();
+              } else {
+                System.out.print("ERROR: Server is already listening.");
+              }
+              break;
+            case "getport":
+              System.out.println(server.getPort());
+              break;
+          }
+        } else { // **** Changed for E50 KL
+          this.display(message);
+        }
+      }
+    } catch (Exception ex) {
+      System.out.println("Unexpected error while reading from console!");
+    }
+  }
+
+  public void display(String message) {
+    server.sendToAllClients("SERVER MSG> " + message);
+    System.out.println("SERVER MSG> " + message);
+  }
+
+  public static void main(String[] args) {
+    int port; // The port number
+
+    try {
+      port = Integer.parseInt(args[0]); // **** Changed for E50 KL
+    } catch (ArrayIndexOutOfBoundsException e) {
+      port = 7125; //default port number (7 + last 3 digits of student number)
+    }
+    ServerConsole console = new ServerConsole(port);
+    console.accept(); // Wait for console data
+  }
+}
